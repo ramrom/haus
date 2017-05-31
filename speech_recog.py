feb12bc7f2e9b1cd19773b60d3c0cc38a8d94c21
Sreeram Mittapalli <smittapalli@enovafinancial.com>
2017-05-31 01:00:55 -0500

    add gcloud oauth

--- speech_recog.py
+++ speech_recog.py
@@ -1,10 +1,19 @@
 #!/usr/local/bin/python
-# speech recog
-#import google-api-python-client
 import pdb
-f = open('/users/smittapalli/.creds/gcloud_api_key','r')
-API_KEY = f.read()
+#import google-api-python-client
+from apiclient.discovery import build
+
+#import subprocess
+#task = subprocess.Popen("cat file.log | tail -1", shell=True, stdout=subprocess.PIPE)
+#data = task.stdout.read()
+
+f = open('/users/smittapalli/.creds/gcloud_oauth','r')
+OAUTH = f.readlines()
+OAUTH_CLIENT_ID = OAUTH[0][0:-1]
+OAUTH_SECRET = OAUTH[1][0:-1]
 f.close()
+
+pdb.set_trace()
 
 def g_sp_recog():
   print 'hi'
