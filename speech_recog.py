aaf6f62cfe47bb0f9f563602e32079acf6997adc
Sreeram Mittapalli <smittapalli@enovafinancial.com>
2017-06-01 20:31:51 -0500

    add g speech recog

--- speech_recog.py
+++ speech_recog.py
@@ -13,7 +13,43 @@ OAUTH_CLIENT_ID = OAUTH[0][0:-1]
 OAUTH_SECRET = OAUTH[1][0:-1]
 f.close()
 
-pdb.set_trace()
+#pdb.set_trace()
 
-def g_sp_recog():
-  print 'hi'
+import base64
+import json
+
+from googleapiclient import discovery
+import httplib2
+from oauth2client.client import GoogleCredentials
+
+
+DISCOVERY_URL = ('https://{api}.googleapis.com/$discovery/rest?'
+                 'version={apiVersion}')
+
+
+def get_speech_service():
+  credentials = GoogleCredentials.get_application_default().create_scoped(
+      ['https://www.googleapis.com/auth/cloud-platform'])
+  http = httplib2.Http()
+  credentials.authorize(http)
+  return discovery.build('speech', 'v1beta1', http=http, discoveryServiceUrl=DISCOVERY_URL)
+
+
+def main(speech_file):
+  with open(speech_file, 'rb') as speech:
+    speech_content = base64.b64encode(speech.read())
+
+  service = get_speech_service()
+  service_request = service.speech().syncrecognize(
+    body={
+      'config': {
+        'encoding': 'LINEAR16',  # raw 16-bit signed LE samples
+        'sampleRate': 16000,  # 16 khz
+        'languageCode': 'en-US',  # a BCP-47 language tag
+      },
+    'audio': {
+      'content': speech_content.decode('UTF-8')
+      }
+    })
+  response = service_request.execute()
+  print(json.dumps(response))
