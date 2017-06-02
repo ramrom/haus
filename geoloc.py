22ea3bd71b736f74c15490c439425c55a87a0504
Sreeram Mittapalli <smittapalli@enovafinancial.com>
2017-06-02 18:21:00 -0500

    geoloc can grab address from the lat long

--- geoloc.py
+++ geoloc.py
@@ -1,9 +1,20 @@
 #!/usr/local/bin/python
 import requests
+import json
+import pdb
 
 with open('/users/smittapalli/.creds/gcloud_geoloc_key','r') as credfile:
   API_KEY = credfile.readlines()[0][0:-1]
 
-URL = "https://www.googleapis.com/geolocation/v1/geolocate?key={0}".format(API_KEY)
-resp = requests.post(URL)
-print resp.json()
+LONGLAT_URL = "https://www.googleapis.com/geolocation/v1/geolocate?key={0}".format(API_KEY)
+resp = requests.post(LONGLAT_URL)
+resp_cont = resp.json()
+
+with open('/users/smittapalli/.creds/gcloud_geocode_key','r') as credfile:
+  API_KEY2 = credfile.readlines()[0][0:-1]
+
+#pdb.set_trace()
+ADDRESS_URL = "https://maps.googleapis.com/maps/api/geocode/json?latlng={1},{2}&key={0}".format(API_KEY2, resp_cont['location']['lat'], resp_cont['location']['lng'])
+resp2 = requests.post(ADDRESS_URL)
+#pdb.set_trace()
+print json.dumps(resp2.json(), indent=4, sort_keys=True)
