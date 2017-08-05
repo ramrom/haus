4153708963946f0562872cefe173552533046bf0
Sreeram Mittapalli <smittapalli@enovafinancial.com>
2017-08-05 12:27:08 -0500

    update ntgear

--- netgearrtr.py
+++ netgearrtr.py
@@ -1,11 +1,14 @@
 #!/usr/local/bin/python
 
 import pdb
-import pynetgear
+import pynetgear   # https://github.com/balloob/pynetgear
+
+# do dir(object) to get list of public methods
 
 def buildclient(password = 'password', user = 'admin', hostname = 'routerlogin.net', port = 80):
   return pynetgear.Netgear(password, hostname, user, port)
 
 if __name__ == "__main__":
-  #if len(sys.argv) > 1 and sys.argv[1] == '1':
   pdb.set_trace()
+  # client = buildclient()
+  # client.get_attached_devices()
