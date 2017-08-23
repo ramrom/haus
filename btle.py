4b11de3888ad3ea9cf458f28a0b562b50b2366cb
Sreeram Mittapalli <smittapalli@enovafinancial.com>
2017-08-22 23:02:50 -0500

    add comments to btle

--- btle.py
+++ btle.py
@@ -1,8 +1,8 @@
 #!/usr/local/bin/python
 # NOTE: 
 # For pi 3 (on ubuntumate) for python 2.7 to install pybluez package
-# sudo apt-get install libbluetooth-dev (aka BlueZ, the official linux bluetooth protocol stack)
-# sudo pip install pybluez 
+# sudo apt-get install libbluetooth-dev
+# sudo pip install pybluez ((aka BlueZ, the official linux bluetooth protocol stack))
 
 import pdb
 import bluetooth
@@ -16,6 +16,18 @@ def bt_devs():
     print("  %s - %s" % (addr, name))
 
 # bluetooth low energy scan
+# fails with "gattlib not found"
+#   - "pip install gattlib" fails b/c glib-2.0 not found
+#     - "sudp apt-get install libglib2.0-dev"   
+#     - still fails, "fatal error: boost/python/list.hpp: No such file or directory"
+#     - did "sudo apt-get install libboost-python-dev"
+#     - still fails, lboost_thread not found
+#       - see https://raspberrypi.stackexchange.com/questions/55530/pybluez-and-gattlib-error
+#     - installed libboost-thread-dev
+#   - needs root, i get "Set scan parameters failed (are you root?)" error
+# 
+# also see https://home-assistant.io/components/device_tracker.bluetooth_le_tracker/
+
 def btle_devs():
   from bluetooth.ble import DiscoveryService
   
