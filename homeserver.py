0b03a1b7554175d6ca7b790a029c38f2bc6c7e52
Sreeram Mittapalli <sreeram.mittapalli@gmail.com>
2017-09-27 00:21:36 -0500

    add action

--- homeserver.py
+++ homeserver.py
@@ -1,8 +1,11 @@
 # To run: FLASK_APP=homeserver.py flask run -p 8080 --host=192.168.1.102
 
-from flask import Flask
+import os
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/")
 def run():
+  action = request.args.get('act','')
+  os.system('python ~/haus/haus.py light')
   return 'hello world'
